0f29466b4d0a4b350dc89579aee0ce7def65188d
DevOps_Selenium <cybsarynet@gmail.com>
2022-07-07 19:34:40 -0500

    se realiza la carga de estos elementos

--- src/test/java/stepDefinations/LandingPageStepDefiniton.java
+++ src/test/java/stepDefinations/LandingPageStepDefiniton.java
@@ -31,47 +31,19 @@ public class LandingPageStepDefiniton {
 	    public void user_is_on_greencart_landing_page() throws Throwable {
 	       System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
 	       testcontextsetup.driver= new ChromeDriver ();
-				 driver.navigate().to("https://rahulshettyacademy.com/seleniumPractise");
-				 Thread.sleep(10000);
+	       testcontextsetup.driver.get("https://rahulshettyacademy.com/seleniumPractise");
+				 Thread.sleep(50000);
 				 
 	    }
 
 	    @When("user searched with Shortname {string} abnd extract actual name of prodcut")
 	    public void user_searched_with_shortname_something_abnd_extract_actual_name_of_prodcut(String shortName) throws Throwable {
-	        
+	    	Thread.sleep(50000);
 	    	testcontextsetup.driver.findElement(By.xpath("//input[@type='search']")).sendKeys(shortName);
-	    	 landinPageproductName = driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
+	    	testcontextsetup.landinPageproductName = driver.findElement(By.cssSelector("h4.product-name")).getText().split("-")[0].trim();
 	    	System.out.println(landinPageproductName + " '\t is extracted from home page");
 	    	}
 
-	    @Then("user search for {string} shortname in offers page")
-	    public void user_search_for_something_shortname_in_offers_page(String shortName) 
-	    		throws Throwable {
-	       
 	    
-	     	driver.findElement(By.linkText("Top Deals")).click();
-	     	Set <String> s1= driver.getWindowHandles(); //recuento total de ventanass abiertas
-	     	Iterator <String> i1=s1.iterator(); //recupera las ventanas y empeiza a contar
-	     	String parentWindow = i1.next(); //ventana princuoal
-	     	String childWindow= i1.next(); //ventana secundaria
-	     	
-	     	driver.switchTo().window(childWindow);
-	     	driver.findElement(By.xpath("//input[@id='search-field']")).sendKeys(shortName);
-	     	Thread.sleep(3000);
-	     	 offerPage=driver.findElement(By.cssSelector("tr td:nth-child(1)")).getText();
-	     	
-	     	System.out.println(offerPage + "\t Contains same Text");
-	     			     	
-	     	
-	    }
-	    
-	   
-		@Then("Validate product name in offers page matches with Landing Page") 
-	    	
-	    public void validate_product_name_in_offers_page() {
-	    		
-	    		Assert.assertEquals(offerPage,landinPageproductName);
-	    	}
-	    	
 	    	 
 }
--- src/test/java/stepDefinations/OfferPageStepDefiniton.java
+++ src/test/java/stepDefinations/OfferPageStepDefiniton.java
@@ -11,29 +11,34 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import junit.framework.Assert;
+import utils.TestContextSetup;
 
 
 public class OfferPageStepDefiniton {
-	public String landinPageproductName;
+	
 	public String offerPage;
 	public WebDriver driver;
+	TestContextSetup testcontextsetup;
 	
+	public OfferPageStepDefiniton (TestContextSetup testcontextsetup) {
+		this.testcontextsetup=testcontextsetup;
+	}
 	    
 	    @Then("user search for {string} shortname in offers page")
 	    public void user_search_for_something_shortname_in_offers_page(String shortName) 
 	    		throws Throwable {
 	       
-	    
-	     	driver.findElement(By.linkText("Top Deals")).click();
+	    	Thread.sleep(50000);
+	    	testcontextsetup.driver.findElement(By.linkText("Top Deals")).click();
 	     	Set <String> s1= driver.getWindowHandles(); //recuento total de ventanass abiertas
 	     	Iterator <String> i1=s1.iterator(); //recupera las ventanas y empeiza a contar
 	     	String parentWindow = i1.next(); //ventana princuoal
 	     	String childWindow= i1.next(); //ventana secundaria
 	     	
-	     	driver.switchTo().window(childWindow);
-	     	driver.findElement(By.xpath("//input[@id='search-field']")).sendKeys(shortName);
+	     	testcontextsetup.driver.switchTo().window(childWindow);
+	     	testcontextsetup.driver.findElement(By.xpath("//input[@id='search-field']")).sendKeys(shortName);
 	     	Thread.sleep(3000);
-	     	 offerPage=driver.findElement(By.cssSelector("tr td:nth-child(1)")).getText();
+	     	 offerPage=testcontextsetup.driver.findElement(By.cssSelector("tr td:nth-child(1)")).getText();
 	     	
 	     	System.out.println(offerPage + "\t Contains same Text");
 	     			     	
@@ -45,7 +50,7 @@ public class OfferPageStepDefiniton {
 	    	
 	    public void validate_product_name_in_offers_page() {
 	    		
-	    		Assert.assertEquals(offerPage,landinPageproductName);
+	    		Assert.assertEquals(offerPage,testcontextsetup.landinPageproductName);
 	    	}
 	    	
 	    	 
