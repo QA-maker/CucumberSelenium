909e7539099ca8dbf8ed8f04bd757a8b160d10e2
DevOps_Selenium <cybsarynet@gmail.com>
2022-07-13 19:34:10 -0500

    Crear archivos de propiedades globales

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -1,5 +1,10 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,14 +12,30 @@ public class TestBase {
 	
 	public WebDriver driver;
 	
-	public WebDriver  WebDriverManager() {
+	public WebDriver  WebDriverManager() throws IOException {
 		
-		if(driver == null) {
-		System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//test//resources//global.properties");
+		Properties prop= new Properties();
+		prop.load(fis);
+		String url =prop.getProperty("QAUrl");
+		if(driver==null) {
+		
+		if(prop.getProperty("browser") == "chrome") {//cuando eldriver no esta cargado
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//src//test//resources//chromedriver.exe");
 	       driver= new ChromeDriver ();
-	       driver.get("https://rahulshettyacademy.com/seleniumPractise");
+	       
 		}
-			return driver;
+		
+		if(prop.getProperty("browser") == "firefox") {
+			
+			//firefox code
+		}
+		
+		driver.get(url);
 	}
-
+		return driver;
+}	
+			
+	
+	
 }
